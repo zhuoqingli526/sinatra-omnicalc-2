c2672180c372a4534bf1658d8a96e1708c3a1d62
zhuoqingli <143445503+zhuoqingli526@users.noreply.github.com>
2025-04-20 21:06:33 +0000

    final_version

--- app.rb
+++ app.rb
@@ -5,12 +5,13 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
-  @first_num = params.fetch("first_num").to_f
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
 
   @result = @first_num + @second_num
   erb(:add_result)
@@ -20,18 +21,23 @@ get("/subtract") do
   erb(:sub_form)
 end
 
-get("/wizard_subtract") do
-  @first_num = params.fetch("first_num").to_f
-  @second_num = params.fetch("second_num").to_f
+get("/wizard_sub") do
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
 
+  @result = @second_num - @first_num 
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
+
+  @result = @second_num * @first_num 
   erb(:mult_result)
 end
 
@@ -40,5 +46,9 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
+  @first_num = params.fetch("first_number").to_f
+  @second_num = params.fetch("second_number").to_f
+
+  @result =  @first_num / @second_num 
   erb(:div_result)
 end
